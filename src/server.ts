b0b9a37c7147ed6c388f91f1afadfef4b3ba2ba4
Mateus Martins <mateus.a.n.martins@gmail.com>
2022-05-19 22:06:39 -0300

    removendo regra cors

--- src/server.ts
+++ src/server.ts
@@ -4,20 +4,9 @@ import cors from 'cors'
 
 const app = express();
 
-var whitelist = ['https://feedback-widget-nlw-web.vercel.app']
-var corsOptions = {
-    origin: function (origin: any, callback: any) {
-        if (whitelist.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}
-
-app.use(cors(corsOptions));
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb' }));
+app.use(cors());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
