edf7ef88c69370fd7b07892aac31f35b6b7d3d25
Mateus Martins <mateus.a.n.martins@gmail.com>
2022-05-19 21:59:47 -0300

    adicionando regra cors

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,20 @@ import cors from 'cors'
 
 const app = express();
 
-app.use(cors());
-app.use(express.json({limit: '50mb'}));
-app.use(express.urlencoded({limit: '50mb'}));
+var whitelist = ['https://feedback-widget-nlw-server-production.up.railway.app/']
+var corsOptions = {
+    origin: function (origin: any, callback: any) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+}
+
+app.use(cors(corsOptions));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb' }));
 app.use(routes)
 
 app.listen(process.env.PORT || 3333, () => {
